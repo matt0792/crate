66107d99611424fd4749d5a28282ac765e90d2d4
Matthew Thompson <matthew.thompson@velocitytrade.com>
2026-05-02 14:42:08 +0200

    add api comments

--- crate/crate.go
+++ crate/crate.go
@@ -26,32 +26,41 @@ func Store[T any](namespace string, obj T) (string, error) {
 }
 
 // Update updates an object by its id.
-// Returns a boolean indicating success.
+// Returns false if the id does not exist.
 func Update[T any](namespace, id string, obj T) bool {
 	return internal.Update(namespace, id, obj)
 }
 
 // Get retrieves an object by id.
+// Returns false if the id does not exist.
 func Get[T any](namespace, id string) (T, bool) {
 	return internal.Get[T](namespace, id)
 }
 
+// Find returns all objects in a namespace that satisfy the filter.
+// Returns an empty slice if no objects match.
 func Find[T any](namespace string, filter func(obj T) bool) []T {
 	return internal.Find(namespace, filter)
 }
 
+// FindPaged returns a page of objects that satisfy the filter.
+// skip is the number of matching results to skip, limit is the maximum to return.
 func FindPaged[T any](namespace string, skip, limit int, filter func(obj T) bool) []T {
 	return internal.FindPaged(namespace, skip, limit, filter)
 }
 
+// Delete removes an object by id.
 func Delete(namespace, id string) {
 	internal.Delete(namespace, id)
 }
 
+// DeleteBy removes all objects in a namespace that satisfy the filter.
+// Returns the number of objects deleted.
 func DeleteBy[T any](namespace string, filter func(obj T) bool) int {
 	return internal.DeleteBy(namespace, filter)
 }
 
+// Namespaces returns the names of all existing namespaces.
 func Namespaces() []string {
 	return internal.Namespaces()
 }
